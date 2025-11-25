3806140044630b4dcd694ff7d4f5bc1d98e14bb8
fqueta <ger.maisaqui1@gmail.com>
2025-11-25 07:01:45 -0300

    25/11/2025

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Services\Qlib;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,23 +20,42 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-          $this->call([
-            UserSeeder::class,
-            // escolaridadeSeeder::class,
-            // estadocivilSeeder::class,
-            // ProfissaoSeeder::class,
-            MenuSeeder::class, //cadastra menus permissõs e menu_permissions
-            // PermissionSeeder::class,
-            // MenuPermissionSeeder::class,
-            DashboardMetricsSeeder::class,
-            CategorySeeder::class,
-            FinancialCategoriesSeeder::class,
-            OptionsTableSeeder::class,
-            ProductUnitsSeeder::class,
-            FunnelStageSeeder::class,
-            AircraftAttendanceSeeder::class,
-            AeronavesSeeder::class,
-            // QoptionSeeder::class,
-        ]);
+        if(Qlib::is_crm_aero()){
+            $var_cal = [
+                    UserSeeder::class,
+                    // escolaridadeSeeder::class,
+                    // estadocivilSeeder::class,
+                    // ProfissaoSeeder::class,
+                    MenuSeeder::class, //cadastra menus permissõs e menu_permissions
+                    // PermissionSeeder::class,
+                    // MenuPermissionSeeder::class,
+                    DashboardMetricsSeeder::class,
+                    CategorySeeder::class,
+                    FinancialCategoriesSeeder::class,
+                    OptionsTableSeeder::class,
+                    ProductUnitsSeeder::class,
+                    FunnelStageSeeder::class,
+                    AircraftAttendanceSeeder::class,
+                    AeronavesSeeder::class,
+                    CursosSeeder::class,
+                    // QoptionSeeder::class,
+            ];
+
+        }else{
+            $var_cal = [
+                    UserSeeder::class,
+                    MenuSeeder::class, //cadastra menus permissõs e menu_permissions
+                    DashboardMetricsSeeder::class,
+                    CategorySeeder::class,
+                    FinancialCategoriesSeeder::class,
+                    OptionsTableSeeder::class,
+                    ProductUnitsSeeder::class,
+                    FunnelStageSeeder::class,
+                    AircraftAttendanceSeeder::class,
+                    AeronavesSeeder::class,
+            ];
+
+        }
+        $this->call($var_cal);
     }
 }
